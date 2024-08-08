318e23827e7212bb0df6ff4a766dd5ba306c1b51
Xueying Wang <wang.xy.326@gmail.com>
2024-08-08 20:23:04 +0200

    add chains data examples

---
+++ examples/steward/chains-list.ts
@@ -0,0 +1,26 @@
+import {
+  OcelloidsClient,
+  type steward as types,
+} from "@sodazone/ocelloids-client";
+
+// Instantiate an Ocelloids Client
+const steward = new OcelloidsClient({
+  apiKey: process.env.OC_API_KEY,
+}).agent("steward");
+
+// Get the assets metadata from multiple networks
+try {
+  const { items } = await steward.query<
+    types.StewardQueryArgs,
+    types.AssetMetadata
+  >({
+    
+    op: "chains.list"
+  }, {
+    limit: 100
+  });
+
+  console.log("CHAIN DATA:\n", items);
+} catch (error) {
+  console.error(error);
+}
---
+++ examples/steward/chains.ts
@@ -0,0 +1,33 @@
+import {
+  OcelloidsClient,
+  type steward as types,
+} from "@sodazone/ocelloids-client";
+
+// Instantiate an Ocelloids Client
+const steward = new OcelloidsClient({
+  apiKey: process.env.OC_API_KEY,
+}).agent("steward");
+
+// Get the assets metadata from multiple networks
+try {
+  const { items } = await steward.query<
+    types.StewardQueryArgs,
+    types.AssetMetadata
+  >({
+    op: "chains",
+    criteria: {
+      networks: [
+        "urn:ocn:polkadot:0",
+        "urn:ocn:polkadot:1000",
+        "urn:ocn:polkadot:1002",
+        "urn:ocn:polkadot:2004",
+        "urn:ocn:polkadot:2030",
+        "urn:ocn:polkadot:2034",
+      ],
+    },
+  });
+
+  console.log("CHAIN DATA:\n", items);
+} catch (error) {
+  console.error(error);
+}
