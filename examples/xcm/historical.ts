9e7ced8b54cc93eab4def9357fac0848a7fc845e
Marc Fornos <marc.fornos@gmail.com>
2025-04-25 11:02:14 +0200

    allow args in xcm historical example

--- examples/xcm/historical.ts
+++ examples/xcm/historical.ts
@@ -1,6 +1,13 @@
+import { parseArgs } from "node:util";
+
 import { createXcmAgent } from "@sodazone/ocelloids-client";
 
 import { formatXcmMessage } from "./util";
+
+const { positionals } = parseArgs({
+  args: Bun.argv,
+  allowPositionals: true,
+});
 
 const agent = createXcmAgent({
   httpUrl: process.env.OC_HTTP_URL,
@@ -8,20 +15,22 @@ const agent = createXcmAgent({
   apiKey: process.env.OC_API_KEY,
 });
 
+const timeframe = positionals.length > 2 ? positionals[2] : "previous_2_hours";
+
 agent.subscribe(
   {
     destinations: "*",
     origins: "*",
     history: {
-      timeframe: "previous_2_hours",
+      timeframe,
     },
   },
   {
     onMessage: (msg) => {
       console.log(formatXcmMessage(msg));
     },
-    onClose: ({reason}) => {
+    onClose: ({ reason }) => {
       console.log(reason);
-    }
+    },
   },
 );
